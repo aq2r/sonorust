5425d0cc827ce5a213c83196e48659c3a7bddcd0
aq2r <183514119+aq2r@users.noreply.github.com>
2025-02-06 20:24:14 +0900

    Sbv2RustDownloads

--- crates/infer_api/src/lib.rs
+++ crates/infer_api/src/lib.rs
@@ -9,3 +9,4 @@ pub use sbv2_pythonclient::errors::Sbv2PythonError;
 
 pub use sbv2_rustclient::client::{Sbv2RustClient, Sbv2RustModel};
 pub use sbv2_rustclient::errors::Sbv2RustError;
+pub use sbv2_rustclient::downloads::Sbv2RustDownloads;
